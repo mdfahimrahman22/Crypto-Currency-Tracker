92f80d8b3cb1b7c90060219720005ff318ef8001
Md. Fahim Rahman <mdfahimrahman22@gmail.com>
2024-11-18 22:01:30 -0500

    issue fixed

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://13.233.50.85:8093"
+    "http://13.233.50.85:8093",
     "https://crypto-tracker.pioneeralpha.com"
 ]
 
